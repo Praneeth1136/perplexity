6df2ff55669f63c06fad5102f9cb5287ca1e91ae
Praneeth1136 <praneethkilaparthi@gmail.com>
2026-06-04 21:57:19 +0530

    langchain service initialization

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./config/database.js";
+import {testAI} from "./services/ai.service.js";
 
 
 const PORT = process.env.PORT || 3000;
+
+testAI();
 
 connectDB()
     .then(()=>{
