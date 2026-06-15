6f7ff41897ebc7a9f3b2745f2541fac4a1c3ba3d
Praneeth1136 <praneethkilaparthi@gmail.com>
2026-06-15 14:45:11 +0530

    feat: initialize socket.io server with CORS configuration and connection handler

--- backend/src/sockets/server.socket.js
+++ backend/src/sockets/server.socket.js
@@ -5,7 +5,7 @@ let io;
 export function initSocket(httpServer) {
     io = new Server(httpServer, {
         cors: {
-            origin: ["http://localhost:5173" , "http://localhost:5174" , process.env.FRONTEND_URL],
+            origin: [ process.env.FRONTEND_URL],
             credentials: true,
 
         }
